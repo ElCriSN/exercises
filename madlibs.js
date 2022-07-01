5441861940c1b686adea71134471f595596c49c3
ElCriSN <cristian.faundezp@utem.cl>
2022-07-01 18:49:19 -0400

    freeCodeCamp exercise =)!!!! 👍👍👍!
    🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍!!

---
+++ madlibs.js
@@ -0,0 +1,7 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "The " + myAdjective + " friendly " + myNoun + " is cooool and " + myVerb + " very " + myAdverb;
+console.log(wordBlanks);
