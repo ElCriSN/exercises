83c40edb4db438f65c5bfcb4ac24e33bed507a86
ElCriSN <cristian.faundezp@utem.cl>
2022-07-01 19:10:37 -0400

    freeCodeCamppp ahhahaaa 👍👍👍!!
    👼👼👼😀🐱‍🏍🐱‍🏍🐱‍🏍!!!

---
+++ todolist.js
@@ -0,0 +1,6 @@
+const myList = [["Bananas", 10], ["Apples", 8], ["Coool Fruits", 200], ["Bread", 2], ["Confort", 8]];
+console.log("Let's to get " + myList[0][0] + " , " + myList[0][1] + " it's cooool :)!!!! " );
+console.log("Let's to get " + myList[1][0] + " , " + myList[1][1] + " it's cooool :)!!!! " );
+console.log("Let's to get " + myList[2][0] + " , " + myList[2][1] + " it's cooool :)!!!! " );
+console.log("Let's to get " + myList[3][0] + " , " + myList[3][1] + " it's cooool :)!!!! " );
+console.log("Let's to get " + myList[4][0] + " , " + myList[4][1] + " it's cooool :)!!!! " );
