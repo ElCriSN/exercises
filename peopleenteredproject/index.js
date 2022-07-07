2cc7d65f022b10546daf26ac39510a6dc357fb0d
ElCriSN <cristian.faundezp@utem.cl>
2022-07-07 11:56:35 -0400

    😀😀!
    Finished :P thanks to Scrimbaaaa hahahahaa 😁😁😁😁!!

--- peopleenteredproject/index.js
+++ peopleenteredproject/index.js
@@ -1,10 +1,18 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
-console.log(countEl)
-
-let count = 0
-
 function increment() {
-        count = count + 1
-        countEl.innerText = count
-    }
+        count += 1
+        countEl.textContent = count
+}
+
+//1. Create a function Save(), which logs out the count when it's called
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway hahahaa =DDD!! =)!!")
